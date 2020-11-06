637048932aecfeec930b7248fa2d0284a04a7dd8
Asad Nadeem <asad.ciit015@gmail.com>
2020-11-06 23:11:01 +0500

    Updates

---
+++ precache-manifest.1520d91d87dbf4b648ebb42ea8bd1c37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdfaf621136b3fb7597e3e6f8bb01060",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "25dad04c434c4d4eba2b",
+    "url": "/todo-app/static/css/main.65c3ba5e.chunk.css"
+  },
+  {
+    "revision": "412b652e98f114b064eb",
+    "url": "/todo-app/static/js/2.b62e5c2e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-app/static/js/2.b62e5c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25dad04c434c4d4eba2b",
+    "url": "/todo-app/static/js/main.469d1ca5.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);
